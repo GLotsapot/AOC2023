79245be05043467fd867f71460fe86a1c2f9f758
GLotsapot <GLotsapot@users.noreply.github.com>
2023-12-02 15:18:05 -0500

    Day 1 Part 1

---
+++ Day1_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+Regex regex = new Regex(@"\d");
+int answer = 0;
+
+var input = File.ReadLines("input.txt");
+foreach (var line in input)
+{
+    var matches = regex.Matches(line);
+    var first = Int32.Parse(matches.First().Value);
+    var last = Int32.Parse(matches.Last().Value);
+    answer += first * 10 + last;
+} 
+Console.WriteLine(answer);
